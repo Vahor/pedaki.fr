4b4f8be56ad9701edd88c99254a116d6d22f0709
Vahor <nathan.d0601@gmail.com>
2023-07-30 23:08:57 +0200

    fix padding mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,12 +5,28 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      <header className="sticky inset-x-0 top-2 z-50 mt-8">
+      <div>
+        {/* Grid bg */}
+        <div
+          aria-hidden
+          className="absolute inset-x-0 top-0 z-[-1] mx-auto h-96 bg-grid-gray-200"
+          style={{
+            backgroundPositionX: '-16px',
+          }}
+        >
+          <div className="h-full w-full bg-gradient-to-b from-transparent via-gray-50 to-gray-50"></div>
+        </div>
+
+        <div className="pt-8"></div>
+      </div>
+      <header className="sticky inset-x-0 top-2 z-50">
         <div
           className="container mx-auto flex w-full max-w-screen-xl items-center justify-center md:w-max"
-          style={{
-            paddingRight: 'calc(2rem + var(--removed-body-scroll-bar-size, 0px))',
-          }}
+          style={
+            {
+              // paddingRight: 'calc(2rem + var(--removed-body-scroll-bar-size, 0px))',
+            }
+          }
         >
           <div className="relative flex w-full flex-col rounded-lg bg-white px-4 py-2 shadow-xl">
             <div className="flex justify-between gap-4">
@@ -21,16 +37,6 @@ const Header = () => {
           </div>
         </div>
       </header>
-      {/* Grid bg */}
-      <div
-        aria-hidden
-        className="absolute inset-x-0 top-0 z-[-1] mx-auto h-96 bg-grid-gray-200"
-        style={{
-          backgroundPositionX: '-16px',
-        }}
-      >
-        <div className="h-full w-full bg-gradient-to-b from-transparent via-gray-50 to-gray-50"></div>
-      </div>
     </>
   );
 };
