143d2a7e9bb2e13ca74d0ab52bb2e002e34b7a87
Vahor <nathan.d0601@gmail.com>
2023-08-12 22:41:53 +0200

    I forgor

--- apps/www/src/app/(home)/features.tsx
+++ apps/www/src/app/(home)/features.tsx
@@ -79,7 +79,7 @@ const FeatureCard: React.FC<{
   alt: string;
 }> = ({ title, description, link, learnMore, alt, video }) => {
   return (
-    <div className="flex items-center gap-4 lg:flex-row lg:gap-16 lg:even:flex-row-reverse">
+    <div className="flex items-center gap-4 flex-col lg:flex-row lg:gap-16 lg:even:flex-row-reverse">
       <div className="relative space-y-4 p-0.5">
         <div className="h-10 w-10 rounded-full bg-primary/50">{/*  TODO icon per feature */}</div>
         <CardTitle className="">{title}</CardTitle>
