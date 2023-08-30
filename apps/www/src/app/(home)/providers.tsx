a712ac14f9a876d31aec5baea3a7f9293aace950
Vahor <nathan.d0601@gmail.com>
2023-08-30 18:39:22 +0200

    remove sonner

--- apps/www/src/app/(home)/providers.tsx
+++ apps/www/src/app/(home)/providers.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { Provider as BalancerProvider } from 'react-wrap-balancer';
-import { Toaster } from 'sonner';
 
 interface Props {
   children: (React.ReactElement | React.ReactNode) | (React.ReactElement | React.ReactNode)[];
@@ -11,7 +10,6 @@ interface Props {
 export const Providers = ({ children }: Props) => {
   return (
     <>
-      <Toaster />
       <BalancerProvider>{children}</BalancerProvider>
     </>
   );
