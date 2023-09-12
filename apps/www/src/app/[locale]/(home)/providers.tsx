8eb1da3b3e78563f63bc9aa2185f95580e5de59d
Vahor <nathan.d0601@gmail.com>
2023-09-12 22:26:03 +0200

    add Toaster provider

--- apps/www/src/app/[locale]/(home)/providers.tsx
+++ apps/www/src/app/[locale]/(home)/providers.tsx
@@ -5,6 +5,7 @@ import fr from '~/locales/fr';
 import type { BaseLocale } from 'international-types';
 import React from 'react';
 import { Provider as BalancerProvider } from 'react-wrap-balancer';
+import { Toaster } from 'sonner';
 
 interface Props {
   children: (React.ReactElement | React.ReactNode) | (React.ReactElement | React.ReactNode)[];
@@ -14,6 +15,7 @@ export const Providers = ({ children }: Props) => {
   // TODO: fix usage of `as unknown as BaseLocale`, related to the use of object notation in the lang files
   return (
     <>
+      <Toaster />
       <BalancerProvider>
         <I18nProviderClient fallbackLocale={fr as unknown as BaseLocale}>
           {children}
