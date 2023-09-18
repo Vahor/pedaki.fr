992f6baa7c31b6794a7e3a766ebae613226ba7d9
Vahor <nathan.d0601@gmail.com>
2023-09-18 21:53:36 +0200

    shrek is life

--- apps/www/src/app/about/fonts.tsx
+++ apps/www/src/app/about/fonts.tsx
@@ -1,9 +1,8 @@
-import { cn } from '@pedaki/design/utils';
+import { Card } from '@pedaki/design/ui/card';
 import SectionTitle from '~/components/section/SectionTitle';
 import React from 'react';
 
 const Fonts = () => {
-  // TODO: font du logo
   return (
     <section>
       <SectionTitle anchor="assets">Fonts</SectionTitle>
@@ -12,16 +11,20 @@ const Fonts = () => {
       </p>
       <div className="grid grid-cols-1 gap-x-8 gap-y-2 md:grid-cols-2">
         <Font
-          title="Opens sans"
+          title="OpenSans"
           fontVar="--font-sans"
-          descriptionClassName="text-primary-foreground"
           description="Police utilisée sur la majorité du site"
+          content="Somebody once told me\n
+The world is gonna roll me\n
+I ain't the sharpest tool in the shed"
         />
         <Font
-          title="Opens sans mono"
+          title="FiraCode"
           fontVar="--font-mono"
-          descriptionClassName="text-primary-foreground"
-          description="Police utilisée pour le code"
+          description="Police utilisée pour les éléments de code"
+          content="She was looking kind of dumb\n
+With her finger and her thumb\n
+In the shape of an 'L' on her forehead"
         />
       </div>
     </section>
@@ -32,23 +35,28 @@ const Font = ({
   title,
   description,
   fontVar,
-  className,
-  descriptionClassName,
+  content,
 }: {
   title: string;
   description: string;
   fontVar: string;
-  className?: string;
-  descriptionClassName?: string;
+  content: string;
 }) => {
   return (
-    <div className={cn('rounded-lg border bg-secondary p-2', className)}>
-      <p className="text-foreground mr-2 text-lg font-medium">{title}</p>
-      <p className={cn('text-sm', descriptionClassName)}>{description}</p>
-      <p style={{ fontFamily: `var(${fontVar})` }} className="mt-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    <Card className="p-4">
+      <div className="mb-2">
+        <h4 className="text-lg font-medium text-primary">{title}</h4>
+        <p className="text-xs text-secondary">{description}</p>
+      </div>
+      <p style={{ fontFamily: `var(${fontVar})` }}>
+        {content.split('\\n').map(line => (
+          <React.Fragment key={line}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
       </p>
-    </div>
+    </Card>
   );
 };
 
