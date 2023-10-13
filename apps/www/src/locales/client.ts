34eff24f8f57de26db6e7f491eb344a23dc344d5
Clément RICATTE <clement.ricatte57@gmail.com>
2023-10-13 11:06:09 +0200

    feat: :sparkles: bump next-international version (#54)

--- apps/www/src/locales/client.ts
+++ apps/www/src/locales/client.ts
@@ -1,7 +1,13 @@
 import { createI18nClient } from 'next-international/client';
+import fr from './fr';
 
 export const { useI18n, useScopedI18n, I18nProviderClient, useCurrentLocale, useChangeLocale } =
-  createI18nClient({
-    fr: () => import('./fr'),
-    en: () => import('./en'),
-  });
+  createI18nClient(
+    {
+      fr: () => import('./fr'),
+      en: () => import('./en'),
+    },
+    {
+      fallbackLocale: fr,
+    },
+  );
