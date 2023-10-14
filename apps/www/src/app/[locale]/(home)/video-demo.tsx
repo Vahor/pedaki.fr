221cdcd62950464140720a964298253aa6147e83
Vahor <nathan.d0601@gmail.com>
2023-10-14 16:05:10 +0200

    animation

--- apps/www/src/app/[locale]/(home)/video-demo.tsx
+++ apps/www/src/app/[locale]/(home)/video-demo.tsx
@@ -1,10 +1,11 @@
 import { AspectRatio } from '@pedaki/design/ui/aspect-ratio';
 import { Dialog, DialogContent, DialogTrigger } from '@pedaki/design/ui/dialog';
 import mock from '@public/a/mock-hero.webp';
-import { getCurrentLocale } from '~/locales/server';
 import type { LocaleCode } from '~/locales/server';
+import { getCurrentLocale } from '~/locales/server';
 import Image from 'next/image';
 import React from 'react';
+
 
 const DesktopSvg = ({ locale }: { locale: LocaleCode }) => {
   switch (locale) {
@@ -173,22 +174,31 @@ const Arrow = () => {
 
 const VideoDemo = () => {
   return (
-    <section className="relative mx-auto mt-20 w-full max-w-screen-xl px-8 md:mt-14 xl:px-4 2xl:px-0">
+    <section className="relative mx-auto mt-20 w-full max-w-screen-xl px-8 md:mt-14 md:px-24 2xl:px-0">
       <div className="pointer-events-none absolute inset-x-0 bottom-0 z-[2] h-32 bg-gradient-to-b from-transparent to-white to-90% sm:h-64"></div>
       <Arrow />
       <Dialog>
         <DialogTrigger
           aria-label="Play video"
-          className="group relative flex w-full cursor-pointer"
+          className="relative flex w-full cursor-pointer"
         >
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="flex h-12 w-12 items-center justify-center rounded-full bg-orange shadow-outline shadow-orange/20 transition duration-300 hover:shadow-outline-lg hover:shadow-orange/30 group-hover:scale-110 md:h-16 md:w-16">
-              <svg width="32" height="32" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill="#ffffff"
-                  d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128Z"
-                />
-              </svg>
+          <div className="absolute inset-0 flex items-center justify-center group">
+            <div className="relative flex items-center justify-center">
+              <div className="flex h-12 w-12 z-[1] items-center justify-center rounded-full bg-orange shadow-outline shadow-orange/20 transition duration-300 hover:shadow-outline-lg group-hover:shadow-orange/30 group-hover:scale-110 md:h-16 md:w-16">
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 256 256"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="z-2"
+                >
+                  <path
+                    fill="#ffffff"
+                    d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128Z"
+                  />
+                </svg>
+              </div>
+              <div className="absolute inset-0 z-0 animate-ping rounded-full bg-orange/30 transition" style={{animationDuration: '2000ms'}}></div>
             </div>
           </div>
           <Image src={mock} alt="mock" priority />
