7caf26a488d255a4f98687910536e0b55c12778d
Clément RICATTE <clement.ricatte57@gmail.com>
2023-11-01 13:27:23 +0100

    Reinstate typecheck (#75)

--- apps/www/src/middleware.ts
+++ apps/www/src/middleware.ts
@@ -2,7 +2,7 @@ import { createI18nMiddleware } from 'next-international/middleware';
 import type { NextRequest } from 'next/server';
 
 const I18nMiddleware = createI18nMiddleware({
-  locales: ['en', 'fr'],
+  locales: ['fr', 'en'],
   defaultLocale: 'fr',
   urlMappingStrategy: 'rewriteDefault',
 });
