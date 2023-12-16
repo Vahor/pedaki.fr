4870ad47eb26649d6d7465ad2676c6877aa31b86
AvaN0x <clement.ricatte57@gmail.com>
2023-12-16 21:52:35 +0100

    fix: 🐛 fix i18n cookie on first request
    Co-authored-by: Vahor <me@vahor.fr>

--- apps/www/src/middleware.ts
+++ apps/www/src/middleware.ts
@@ -1,3 +1,4 @@
+import type { LocaleCode } from '~/locales/server';
 import { fallbackLocale, locales } from '~/locales/shared';
 import { createI18nMiddleware } from 'next-international/middleware';
 import type { NextRequest } from 'next/server';
@@ -9,6 +10,14 @@ const I18nMiddleware = createI18nMiddleware({
 });
 
 export function middleware(request: NextRequest) {
+  // Workaround to have to cookie available in the first request
+  const pathname = request.nextUrl.pathname;
+  let localePath = pathname.split('/', 2)[1] as LocaleCode | undefined;
+  localePath = localePath && locales.includes(localePath) ? localePath : undefined;
+
+  const locale = request.cookies.get('Next-Locale')?.value ?? localePath ?? fallbackLocale;
+  request.cookies.set('Next-Locale', locale);
+
   return I18nMiddleware(request);
 }
 
